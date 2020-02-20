745a755d0d6bbec9b7e6a153acd02bce62bb8075
nehleh <kargarfard870@gmail.com>
2020-02-21 10:52:29 +1100

    VB ??

--- JC69_VB.py
+++ JC69_VB.py
@@ -3,6 +3,9 @@ from scipy.stats import gamma
 from scipy.integrate import quad
 import matplotlib.pyplot as plt
 from scipy.special import digamma
+from scipy.special import loggamma
+from scipy.optimize import Bounds
+import scipy.optimize as spo
 
 n = 1000
 x = 100
@@ -10,10 +13,10 @@ alpha = 1
 beta = 1
 
 
+
 def prior(param):
-    d = param
-    pr = gamma.pdf(d, alpha)
-    return pr
+    return gamma(a=alpha, scale=beta, loc=0).pdf(param)
+
 
 
 def p(param):
@@ -32,12 +35,6 @@ def posterior_numerical(param):
     return post
 
 
-def q(param):
-    d = param[0]
-    gam = param[1]
-    gam2 = param[2]
-    return gamma.pdf(d, gam, gam2)
-
 
 def elbo(param):
     d = param[0]
@@ -48,29 +45,37 @@ def elbo(param):
     eq2 = lambda d: np.log(1 / 4 + 3 / 4 * np.exp(-4 / 3 * d))
     two, err2 = quad(eq2, 0, np.inf)
     three = digamma(gam1) - np.log(gam2)
-    total = x * one + (n-x) * two + (alpha - gam1) * three + (gam2 - beta) * gam1/gam2 - gam1 * np.log(gam2) + np.log(gamma(gam1))
-
-
-
+    total = (x * one)  \
+            + ((n-x) * two)  \
+            + (alpha - gam1) * three \
+            + (gam2 - beta) * (gam1/gam2) \
+            - gam1 * np.log(gam2) \
+            + loggamma(gam1)
     return total
 
 
-print(elbo(0.1))
+
+def max_param():
+    initial_guess = [0.2,1,1]
+    bounds =  Bounds([0 , 0 , 0 ], [1, 2, 2])
+    result = spo.minimize(elbo , initial_guess , method='trust-constr' , bounds = bounds, options={'verbose': 1} )
+    print("Result by using scipy:")
+    print("edge_length = {} , gamma1 ={} , gamma2 = {}".format(result.x[0],result.x[1],result.x[2]))
+    return result
 
 
 
+max_param()
 
-
-
-# ll_array = []
-# d = np.arange(0.01, 0.2, 0.002)
-# for i in d:
-#     y_plot = posterior_numerical(i)
-#     ll_array.append(y_plot)
-# plt.plot(d, ll_array, label='true posterior')
-# plt.ylabel('density')
-# plt.xlabel('d')
-# plt.legend(loc='top right')
-# plt.show()
+ll_array = []
+d = np.arange(0.01, 0.2, 0.002)
+for i in d:
+    y_plot = posterior_numerical(i)
+    ll_array.append(y_plot)
+plt.plot(d, ll_array, label='true posterior')
+plt.ylabel('density')
+plt.xlabel('d')
+plt.legend(loc='upper right')
+plt.show()
 
 
