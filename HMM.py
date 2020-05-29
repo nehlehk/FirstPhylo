7d4fc0971bba30a48e03b36034ee5383a6d0d7d5
nehleh <kargarfard870@gmail.com>
2020-05-29 13:07:52 +1000

    expectedLL

--- HMM.py
+++ HMM.py
@@ -5,24 +5,23 @@ import pandas as pd
 
 
 
-df1 = pd.read_csv("/home/nehleh/0_Research/PhD/Data/simulationdata/recombination/faketree/RAxML_perSiteLLs.likelihood_GTR", sep='\s+', header=None)
+df1 = pd.read_csv("/home/nehleh/0_Research/PhD/Data/simulationdata/recombination/500000/partial18.txt", sep='\s+', header=None)
 ll = df1.to_numpy()
 data = np.array(ll)
 X = data.reshape((-1,1))
-X = X[1:100000]
+X = X[1:500000]
 
 mean = np.mean(X)
 std = np.std(X)
 # print(mean)
 # print(std)
 
-# a = float(np.random.normal(mean,std,1))
-# b = float(np.random.normal(mean,std,1))
 
-a= -6.970009569914
-b= mean
-astd= std
-bstd= std
+a = -7.312678532710002
+astd= 1.0286153264950095
+b= -8.026028432710003
+bstd= 1.028615334911007
+
 print(a)
 print(b)
 
@@ -68,12 +67,19 @@ plt.show()
 
 
 
-# newmodel =  hmm.GaussianHMM(n_components=2).fit(X)
+# newmodel =  hmm.GaussianHMM(n_components=2 ,covariance_type="full",params = 'st').fit(X)
+# newmodel.startprob_ = np.array([0.88, 0.12])
+# newmodel.transmat_ = np.array([[0.9999, 0.0001] , [0.0001, 0.9999]])
+# print(newmodel)
 #
-# print("Transitin:")
-# print(newmodel.transmat_)
-# print("emission:")
+# print("emission-means:")
 # print(newmodel.means_)
+# print("emission-covar:")
+# print(newmodel.covars_)
+#
+# print(newmodel.startprob_)
+# print(newmodel.transmat_)
+#
 #
 # posterior = newmodel.predict_proba(X)
 # hiddenStates = newmodel.predict(X)
@@ -90,4 +96,4 @@ plt.show()
 # ax2.plot(posterior)
 # ax2.set_ylabel("posterior probability for each state")
 # plt.show()
-#
+
--- expectedLL.py
+++ expectedLL.py
@@ -6,6 +6,18 @@ import numpy.linalg as la
 
 
 
+pi = [0.2184,0.2606,0.3265,0.1946]
+rates = [2.0431,0.0821,0,0.067,0]
+f = 1
+
+tree = Tree.get_from_path('/home/nehleh/0_Research/PhD/Data/simulationdata/recombination/exampledatset/exampledataset_RAxML_bestTree', 'newick')
+alignment = dendropy.DnaCharacterMatrix.get(file=open("/home/nehleh/0_Research/PhD/Data/simulationdata/recombination/exampledatset/single.fasta"), schema="fasta")
+tips = len(alignment)
+alignment_len = alignment.sequence_size
+
+internalnode = 12 #  -1 works for all internalnodes
+coefficient = 0.7
+
 
 def give_index(c):
     if c == "A":
@@ -65,10 +77,13 @@ def p_matrix(br_length,model):
 
     return p
 # =======================================================================================================
-def expectedLL(tree,dna,model):
+def expectedLL(tree,dna,model,internal_node , coefficient):
+    if internal_node == -1:
+        internal_node = 2 * tips -1
 
     partial = numpy.zeros((2 * tips,4))
     expll = numpy.zeros((2 * tips, 4))
+    recomll = numpy.zeros((2 * tips, 4))
 
     for node in tree.postorder_node_iter():
         node.index = 0
@@ -86,53 +101,69 @@ def expectedLL(tree,dna,model):
                     break
     pos = 0
     for node in tree.postorder_node_iter():
+        if int(node.index) > internal_node:
+            break
         if node.is_leaf():
             i = give_index(str(dna[pos]))
             pos += 1
             partial[node.index][i] = 1
             expll[node.index][i] = 1
+            recomll[node.index][i] = 1
         else:
-            # print(node.index)
             children = node.child_nodes()
             partial[node.index] = numpy.dot(p_matrix(children[0].edge_length,model), partial[children[0].index])
             expll[node.index] = partial[node.index] * children[0].edge_length
+            recomll[node.index] = partial[node.index] * children[0].edge_length * coefficient
             for i in range(1, len(children)):
                 partial[node.index] *= numpy.dot(p_matrix(children[i].edge_length,model), partial[children[i].index])
-                expll[node.index] *= (numpy.dot(p_matrix(children[i].edge_length, model), partial[children[i].index]) * children[i].edge_length)
-
-    expected_ll = numpy.zeros(tips - 1)
-    p_index = 0
-    for par  in range(tips+1,tree.seed_node.index+1,1):
-        temp = 0
-        temp = numpy.sum(expll[par]) * 0.25
-        expected_ll[p_index] = round(numpy.log(temp), 7)
-        p_index += 1
+                expll[node.index] *= (partial[node.index] * children[i].edge_length)
+                recomll[node.index] *=  partial[node.index] * children[i].edge_length * coefficient
 
 
-    return  expected_ll
+    if internal_node == 2 * tips -1 :
+        expected_ll = numpy.zeros(tips - 1)
+        recombination_ll = numpy.zeros(tips - 1)
+        p_index = 0
+        for par  in range(tips+1,tree.seed_node.index+1,1):
+            temp0 = 0
+            temp0 = numpy.sum(expll[par]) * 0.25
+            expected_ll[p_index] = round(numpy.log(temp0), 7)
+            temp1 = 0
+            temp1 = numpy.sum(recomll[par]) * 0.25
+            recombination_ll[p_index] = round(numpy.log(temp1), 7)
+            p_index += 1
+        return expected_ll , recombination_ll
+    else:
+        return round(numpy.log(numpy.sum(expll[internal_node]) * 0.25), 7) , round(numpy.log(numpy.sum(recomll[internal_node]) * 0.25), 7)
+
 #=======================================================================================================================
-def fillvector(tree,alignment,model):
-    expected_vector_a = numpy.zeros((tips - 1 ,alignment_len ))
-    expected_vector_b = numpy.zeros((tips - 1, alignment_len))
+def fillvector(tree,alignment,model , internl_node = -1 , coefficient = 1):
+    recom_vector = numpy.zeros((tips - 1 ,alignment_len ))
+    expected_vector = numpy.zeros((tips - 1, alignment_len))
     for l in range(alignment_len):
         col = ""
         for t in range(tips):
             col += str(alignment[t][l])
-        expected_vector_b[:, l] = expectedLL(tree, col, model)
-    return expected_vector_b
+        expected_vector[:, l] , recom_vector[:, l] = expectedLL(tree, col, model,internl_node , coefficient)
+    return expected_vector, recom_vector
 #=======================================================================================================================
-pi = [0.2184,0.2606,0.3265,0.1946]
-rates = [2.0431,0.0821,0,0.067,0]
-f = 1
-tree = Tree.get_from_path('/home/nehleh/0_Research/PhD/Data/simulationdata/recombination/faketree/RAxML_bestTree.RaxMLtree', 'newick')
-alignment = dendropy.DnaCharacterMatrix.get(file=open("/home/nehleh/0_Research/PhD/Data/simulationdata/recombination/faketree/recombined.fasta"), schema="fasta")
-tips = len(alignment)
-alignment_len = alignment.sequence_size
 
+expll , recomll = fillvector(tree,alignment,'GTR' ,internalnode , coefficient)
 
-result = fillvector(tree,alignment,'GTR')
-for i in range(result.shape[0]):
-    avg = numpy.sum(result[i]) /alignment_len
-    print(" expected likelihood node {}  is: {}".format(i+1+tips, avg))
+if internalnode != -1:
+    avg_exp = numpy.mean(expll)
+    std_exp = numpy.std(expll)
+    print(" expected likelihood node {}  is: {} and std is: {}".format(internalnode, avg_exp, std_exp))
+    avg_recom = numpy.mean(recomll)
+    std_recom = numpy.std(recomll)
+    print(" recombinant mean node {}  is: {} and std is: {}".format(internalnode, avg_recom, std_recom))
+else:
+    for i in range(expll.shape[0]):
+        avg_exp = numpy.mean(expll[i])
+        std_exp = numpy.std(expll[i])
+        avg_recom = numpy.mean(recomll[i])
+        std_recom = numpy.std(recomll[i])
+        print(" expected likelihood node {}  is: {} and std is: {}".format(i+1+tips, avg_exp, std_exp))
+        print(" recombinant mean node {}  is: {} and std is: {}".format(i + 1 + tips, avg_recom, std_recom))
 
 
